57a0917611399d28f18fef6a8eaf5e62d64dfa40
Hansraj <olahansraj2021@gmail.com>
2026-04-16 17:54:00 +0530

    Create superuser automatically

--- DTS/settings.py
+++ DTS/settings.py
@@ -124,3 +124,14 @@ STATIC_URL = '/static/'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_URL = '/admin/login/'
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@gmail.com',
+        password='admin123'
+    )
